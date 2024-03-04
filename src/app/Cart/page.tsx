b818bae9a16a9d9ae08862f6d0e1e547e55f1607
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2024-03-04 18:32:48 +0700

    Add Cart page and update Cashier Menu page

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -1,30 +1,40 @@
-import * as React from 'react';
-import { Box, ThemeProvider } from '@mui/material';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box, ThemeProvider } from '@mui/material';
+
+
+
+
 
 export default function Cart() {
   return (
-    <ThemeProvider
-      theme={{
-        palette: {
-          primary: {
-            main: '#007FFF',
-            dark: '#0066CC',
-          },
-        },
-      }}
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          โต๊ะที่ 1
+        </Typography>
+        <Button color="inherit">Cart</Button>
+      </Toolbar>
+    </AppBar>
+
+    <Box
+      height={70}
+      width="97%"
+      my={4}
+      display="flex"
+      alignItems="center"
+      gap={4}
+      p={2}
+      sx={{ border: '2px solid grey' }}
     >
-      <Box
-        sx={{
-          width: 100,
-          height: 100,
-          borderRadius: 1,
-          bgcolor: 'primary.main',
-          '&:hover': {
-            bgcolor: 'primary.dark',
-          },
-        }}
-      />
-    </ThemeProvider>
+
+        {/* รูปภาพ */}
+        <img src="/public/kp.jpg" alt="kp Image" style={{ width: '100px', height: '100px' }} />
+        {/* ข้อความเพิ่มเติม */}
+        <Typography variant="body1" sx={{ ml: 2 }}>N1 Image</Typography>
+
+    </Box>
+
+  </div>
   );
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useSession, singIn, singOut } from "next-auth/react";
+import { useSession } from "next-auth/react";//singIn, singOut
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
@@ -12,104 +12,55 @@ export default function Login() {
   if (session) {
     <>
       Singed in as {session.user?.email} <br />
-      <button onClick={() => singOut()}>Sing out</button>
+      {/* <button onClick={() => singOut()}>Sing out</button> */}
     </>;
   }
-}
-  const theme = useTheme(); // Access the theme object
 
-<<<<<<< HEAD
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        maxWidth: "400px",
-        margin: "0 auto",
-      }}
-    >
-      <Typography variant="h4" sx={{ marginBottom: "20px" }}>
-        เข้าสู่ระบบ!
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="ชื่อผู้ใช้:"
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          margin="normal"
-          fullWidth
-        />
-        <TextField
-          label="รหัสผ่าน:"
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          margin="normal"
-          fullWidth
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          fullWidth
-          sx={{ backgroundColor: theme.palette.primary.main }} // Use theme object
-        >
-          เข้าสู่ระบบ
-        </Button>
-      </form>
-    </Box>
-  );
+const theme = useTheme(); // Access the theme object
 }
-=======
-//   return (
-//     <Box
-//       sx={{
-//         display: "flex",
-//         flexDirection: "column",
-//         alignItems: "center",
-//         maxWidth: "400px",
-//         margin: "0 auto",
-//       }}
-//     >
-//       <Typography variant="h4" sx={{ marginBottom: "20px" }}>
+// return (
+//   <Box
+//     sx={{
+//       display: "flex",
+//       flexDirection: "column",
+//       alignItems: "center",
+//       maxWidth: "400px",
+//       margin: "0 auto",
+//     }}
+//   >
+//     <Typography variant="h4" sx={{ marginBottom: "20px" }}>
+//       เข้าสู่ระบบ!
+//     </Typography>
+//     <form onSubmit={handleSubmit}>
+//       <TextField
+//         label="ชื่อผู้ใช้:"
+//         type="text"
+//         id="username"
+//         name="username"
+//         value={username}
+//         onChange={(event) => setUsername(event.target.value)}
+//         margin="normal"
+//         fullWidth
+//       />
+//       <TextField
+//         label="รหัสผ่าน:"
+//         type="password"
+//         id="password"
+//         name="password"
+//         value={password}
+//         onChange={(event) => setPassword(event.target.value)}
+//         margin="normal"
+//         fullWidth
+//       />
+//       <Button
+//         type="submit"
+//         variant="contained"
+//         fullWidth
+//         sx={{ backgroundColor: theme.palette.primary.main }} // Use theme object
+//       >
 //         เข้าสู่ระบบ
-//       </Typography>
-//       <form onSubmit={handleSubmit}>
-//         <TextField
-//           label="ชื่อผู้ใช้:"
-//           type="text"
-//           id="username"
-//           name="username"
-//           value={username}
-//           onChange={(event) => setUsername(event.target.value)}
-//           margin="normal"
-//           fullWidth
-//         />
-//         <TextField
-//           label="รหัสผ่าน:"
-//           type="password"
-//           id="password"
-//           name="password"
-//           value={password}
-//           onChange={(event) => setPassword(event.target.value)}
-//           margin="normal"
-//           fullWidth
-//         />
-//         <Button
-//           type="submit"
-//           variant="contained"
-//           fullWidth
-//           sx={{ backgroundColor: theme.palette.primary.main }} // Use theme object
-//         >
-//           เข้าสู่ระบบ
-//         </Button>
-//       </form>
-//     </Box>
-//   );
+//       </Button>
+//     </form>
+//   </Box>
+// );
 // }
->>>>>>> 6f0fa38 (update: page.tsx(login))
